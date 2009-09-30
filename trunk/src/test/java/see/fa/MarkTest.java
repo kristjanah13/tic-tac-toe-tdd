21dc426f649561028eccc7a7de1cf59c9c30a171
franz.see <franz.see@b2682abc-adb6-11de-9e5f-e55ec289a8a8>
2009-09-30 15:22:12 +0000

    Added MarkTest implementation of testing equals().

--- trunk/src/test/java/see/fa/MarkTest.java
+++ trunk/src/test/java/see/fa/MarkTest.java
@@ -5,7 +5,21 @@ import junit.framework.TestCase;
 public class MarkTest extends TestCase {
 	
 	public void testGivenEqualMarkArrays_WhenEquals_ThenTrue() {
+		Mark[] lhs = {Mark.X, Mark.O, Mark.NONE};
+		Mark[] rhs = {Mark.X, Mark.O, Mark.NONE};
 		
+		boolean areMarkArrayEquals = Mark.equals(lhs, rhs);
+		
+		assertTrue("Mark array with same elements should have been equal.", areMarkArrayEquals);
+	}
+	
+	public void testGivenEqual2DMarkArrays_WhenEquals_ThenTrue() {
+		Mark[][] lhs = {{Mark.X}, {Mark.O}, {Mark.NONE}};
+		Mark[][] rhs = {{Mark.X}, {Mark.O}, {Mark.NONE}};
+		
+		boolean are2DMarkArrayEquals = Mark.equals(lhs, rhs);
+		
+		assertTrue("Mark array with same elements should have been equal.", are2DMarkArrayEquals);
 	}
 
 	public void testGivenNonEqualMarkArrays_WhenEquals_ThenFalse() {
