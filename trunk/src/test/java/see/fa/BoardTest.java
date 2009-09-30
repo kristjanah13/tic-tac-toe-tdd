10d287f30bb4e7de7e2fc8f750667a7bb9525860
franz.see <franz.see@b2682abc-adb6-11de-9e5f-e55ec289a8a8>
2009-09-30 16:49:31 +0000

    Refactored test to extract setUp().

--- trunk/src/test/java/see/fa/BoardTest.java
+++ trunk/src/test/java/see/fa/BoardTest.java
@@ -3,30 +3,34 @@ package see.fa;
 import junit.framework.TestCase;
 
 public class BoardTest extends TestCase {
+	
+	private Board board;
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		board = new Board(
+				new Mark[][]{
+						{Mark.X,       Mark.O,      Mark.NONE}, 
+						{Mark.NONE, Mark.X,      Mark.O},
+						{Mark.O,      Mark.NONE, Mark.X}
+				}
+			);
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		board = null;
+		super.tearDown();
+	}
 
 	public void testGivenAPosition_WhenGetRow_ThenReturnRowContainingTheMarksOfThatRow() {
-		Board board = new Board(
-			new Mark[][]{
-					{Mark.X,       Mark.O,      Mark.NONE}, 
-					{Mark.NONE, Mark.X,      Mark.O},
-					{Mark.O,      Mark.NONE, Mark.X}
-			}
-		);
-		
 		Line rowAtPosition = board.getRow(new Position(1,1));
 		
 		assertEquals("Should have retrieved 1st row.", new Line(Mark.X, Mark.O, Mark.NONE), rowAtPosition);
 	}
 	
 	public void testGivenAPosition_WhenGetCol_ThenReturnColContainingTheMarksOfThatCol() {
-		Board board = new Board(
-			new Mark[][]{
-					{Mark.X,       Mark.O,      Mark.NONE}, 
-					{Mark.NONE, Mark.X,      Mark.O},
-					{Mark.O,      Mark.NONE, Mark.X}
-			}
-		);
-		
 		Line colAtPosition = board.getCol(new Position(1,1));
 		
 		assertEquals("Should have retrieved 1st col.", new Line(Mark.X, Mark.NONE, Mark.O), colAtPosition);
