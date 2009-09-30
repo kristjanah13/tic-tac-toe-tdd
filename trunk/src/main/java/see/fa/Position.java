acf8f8b1bdc52840920514a27aec2f1af55f6e3a
franz.see <franz.see@b2682abc-adb6-11de-9e5f-e55ec289a8a8>
2009-09-30 13:13:53 +0000

    Created Position#getX() and Position#getY()

--- trunk/src/main/java/see/fa/Position.java
+++ trunk/src/main/java/see/fa/Position.java
@@ -2,8 +2,20 @@ package see.fa;
 
 public class Position {
 
+	private final int x;
+	private final int y;
+
 	public Position(int x, int y) {
-		
+		this.x = x;
+		this.y = y;
 	}
 
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+	
 }
