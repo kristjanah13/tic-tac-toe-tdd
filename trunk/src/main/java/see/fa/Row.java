48e64d61cd194d8beb71dc6e5d6d9436cb8a435a
franz.see <franz.see@b2682abc-adb6-11de-9e5f-e55ec289a8a8>
2009-09-30 13:59:42 +0000

    Made RowTest pass.

--- trunk/src/main/java/see/fa/Row.java
+++ trunk/src/main/java/see/fa/Row.java
@@ -2,12 +2,21 @@ package see.fa;
 
 public class Row {
 
+	private final Mark[] marks;
+
 	public Row(Mark... marks) {
-		
+		this.marks = marks;
 	}
 
 	public Mark getCommonMark() {
-		return null;
+		Mark commonMark = marks[0];
+		int i = 1;
+		while( i < marks.length && commonMark != null) {
+			commonMark = commonMark == marks[i] ? commonMark : null;
+			i++;
+		}
+		
+		return commonMark;
 	}
 
 }
