d20afb502915553a2dc3646823e594a1d0c7f84e
franz.see <franz.see@b2682abc-adb6-11de-9e5f-e55ec289a8a8>
2009-09-30 14:10:36 +0000

    Added more tests to Result for determining winner.

--- trunk/src/test/java/see/fa/ResultTest.java
+++ trunk/src/test/java/see/fa/ResultTest.java
@@ -4,11 +4,35 @@ import junit.framework.TestCase;
 
 public class ResultTest extends TestCase {
 
-	public void testGivenNullRow_WhenValueOf_ThenReturnNull() {
+	public void testGivenNullRow_WhenValueOf_ThenReturnGameNotFinish() {
 		Row givenNullRow = null;
 		
 		Result result = Result.valueOf(givenNullRow);
 		
 		assertEquals("Game should not have been finished yet based on the row.", Result.GAME_NOT_FINISH, result);
+	}
+	
+	public void testGivenAllXRows_WhenValueOf_ThenReturnXWins() {
+		Row givenAllXRows = new Row(Mark.X, Mark.X, Mark.X);
+		
+		Result result = Result.valueOf(givenAllXRows);
+		
+		assertEquals("X should have won.", Result.X_WINS, result);
+	}
+	
+	public void testGivenAllORows_WhenValueOf_ThenReturnOWins() {
+		Row givenAllXRows = new Row(Mark.O, Mark.O, Mark.O);
+		
+		Result result = Result.valueOf(givenAllXRows);
+		
+		assertEquals("O should have won.", Result.O_WINS, result);
+	}
+	
+	public void testGivenContainsANoneMark_WhenValueOf_ThenReturnGameNotFinish() {
+		Row givenAllXRows = new Row(Mark.NONE, Mark.NONE, Mark.NONE);
+		
+		Result result = Result.valueOf(givenAllXRows);
+		
+		assertEquals("Game should not have been finished yet.", Result.GAME_NOT_FINISH, result);
 	}
 }
