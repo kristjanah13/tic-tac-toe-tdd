8c835fb9d36a9b3a8e36d50583140937bf914dca
franz.see <franz.see@b2682abc-adb6-11de-9e5f-e55ec289a8a8>
2009-09-30 14:30:11 +0000

    Created MarkTest.

---
+++ trunk/src/test/java/see/fa/MarkTest.java
@@ -0,0 +1,23 @@
+package see.fa;
+
+import junit.framework.TestCase;
+
+public class MarkTest extends TestCase {
+	
+	public void testGivenEqualMarkArrays_WhenEquals_ThenTrue() {
+		
+	}
+
+	public void testGivenNonEqualMarkArrays_WhenEquals_ThenFalse() {
+		
+	}
+	
+	public void testHashCodeShouldUseAllMarksToGenerateHashCode() {
+	
+	}
+
+	public void testToStringShouldUseAllMarksToGenerateToString() {
+		
+	}
+
+}
