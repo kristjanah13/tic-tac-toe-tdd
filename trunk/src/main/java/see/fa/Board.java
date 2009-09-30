7995917dcde325570228a52e4f6c09d5475899e9
franz.see <franz.see@b2682abc-adb6-11de-9e5f-e55ec289a8a8>
2009-09-30 13:15:10 +0000

    Overriden Board#equals(..), Board#hashCode(), Board#toString(), and Mark#toString()

--- trunk/src/main/java/see/fa/Board.java
+++ trunk/src/main/java/see/fa/Board.java
@@ -1,8 +1,16 @@
 package see.fa;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
 
 public class Board {
 
+	private static final String COL_SEPARATOR = " ";
+	private static final String ROW_SEPARATOR = "\n";
 	private final Mark[][] marks;
 
 	public Board(Mark[][] marks) {
@@ -23,5 +31,41 @@ public class Board {
 		
 		marks[effectiveRow][effectiveCol] = mark;
 	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if(obj == null || !Board.class.isAssignableFrom(obj.getClass())) {
+			return false;
+		}
+		
+		Board that = (Board)obj;
+		
+		return new EqualsBuilder()
+			.append(this.marks, that.marks)
+			.isEquals();
+	}
+
+	@Override
+	public int hashCode() {
+		HashCodeBuilder hashCodeBuilder = new HashCodeBuilder();
+		hashCodeBuilder.append(marks);
+		return hashCodeBuilder.toHashCode();
+	}
+
+	@Override
+	public String toString() {
+		List<String> rowToStrings = new LinkedList<String>();
+		for(Mark[] row : marks) {
+			rowToStrings.add(StringUtils.join(row, COL_SEPARATOR));
+		}
+		
+		return new StringBuilder()
+			.append(ROW_SEPARATOR)
+			.append(StringUtils.join(rowToStrings, ROW_SEPARATOR))
+			.append(ROW_SEPARATOR)
+			.toString();
+	}
+	
+	
 
 }
--- trunk/src/main/java/see/fa/Mark.java
+++ trunk/src/main/java/see/fa/Mark.java
@@ -2,6 +2,16 @@ package see.fa;
 
 public enum Mark {
 
-	X, NONE
+	X('X'), NONE('_');
 
+	private final char symbol;
+
+	private Mark(char symbol){
+		this.symbol = symbol;
+	}
+
+	@Override
+	public String toString() {
+		return String.valueOf(symbol);
+	}
 }
