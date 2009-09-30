7ab5057766335be022cc9dbee5b87d28f8fa262e
franz.see <franz.see@b2682abc-adb6-11de-9e5f-e55ec289a8a8>
2009-09-30 13:51:51 +0000

    Updated test for new Row#getCommonMark()

--- trunk/src/test/java/see/fa/RowTest.java
+++ trunk/src/test/java/see/fa/RowTest.java
@@ -4,16 +4,16 @@ import junit.framework.TestCase;
 
 public class RowTest extends TestCase {
 	
-	public void testGivenAllContainedMarksAreEqualInputMark_WhenAreAll_ThenTrue() {
-		boolean areAllMarkedAsSuch = new Row(Mark.X, Mark.X, Mark.X).areAll(Mark.X);
+	public void testGivenRowContainsOnlyMarkX_WhenGetCommonMark_ThenMarkX() {
+		Mark commonMark = new Row(Mark.X, Mark.X, Mark.X).getCommonMark();
 		
-		assertTrue("Should have been marked all as the given mark.", areAllMarkedAsSuch);
+		assertEquals("Common mark should have been the only mark that Row only has..", Mark.X, commonMark);
 	}
 	
 	public void testGivenContainsAMarkNotEqualToInputMark_WhenAreAll_ThenFalse() {
-		boolean areAllMarkedAsSuch = new Row(Mark.NONE, Mark.X, Mark.X).areAll(Mark.X);
+		Mark commonMark = new Row(Mark.NONE, Mark.X, Mark.X).getCommonMark();
 		
-		assertFalse("Should NOT have been marked all as the given mark.", areAllMarkedAsSuch);
+		assertNull("Common mark should have been the only mark that Row only has..", commonMark);
 	}
 
 }
