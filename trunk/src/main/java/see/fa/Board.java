0240c26bcb3c6df46a94a9773b46aeee8929db29
franz.see <franz.see@b2682abc-adb6-11de-9e5f-e55ec289a8a8>
2009-09-30 13:12:45 +0000

    Created Board#set(...).

--- trunk/src/main/java/see/fa/Board.java
+++ trunk/src/main/java/see/fa/Board.java
@@ -1,9 +1,27 @@
 package see.fa;
 
+
 public class Board {
 
+	private final Mark[][] marks;
+
 	public Board(Mark[][] marks) {
+		this.marks = marks;
+	}
+
+	public Board() {
+		this(new Mark[][]{
+				{Mark.NONE, Mark.NONE, Mark.NONE},
+				{Mark.NONE, Mark.NONE, Mark.NONE},
+				{Mark.NONE, Mark.NONE, Mark.NONE}
+		});
+	}
+
+	public void set(Position position, Mark mark) {
+		int effectiveRow = position.getX() - 1;
+		int effectiveCol = position.getY() - 1;
 		
+		marks[effectiveRow][effectiveCol] = mark;
 	}
 
 }
