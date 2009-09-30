9fb54d7025c49bec2e3afe3a63f1054003bdaf18
franz.see <franz.see@b2682abc-adb6-11de-9e5f-e55ec289a8a8>
2009-09-30 14:22:25 +0000

    Created BoardTest.

---
+++ trunk/src/test/java/see/fa/BoardTest.java
@@ -0,0 +1,20 @@
+package see.fa;
+
+import junit.framework.TestCase;
+
+public class BoardTest extends TestCase {
+
+	public void testGivenAPosition_WhenGetRow_ThenReturnRowContainingTheMarksOfThatRow() {
+		Board board = new Board(
+			new Mark[][]{
+					{Mark.X,       Mark.O,      Mark.NONE}, 
+					{Mark.NONE, Mark.X,      Mark.O},
+					{Mark.O,      Mark.NONE, Mark.X}
+			}
+		);
+		
+		Row rowAtPosition = board.getRow(new Position(1,1));
+		
+		assertEquals("Should have retrieved 1st row.", new Row(Mark.X, Mark.O, Mark.NONE), rowAtPosition);
+	}
+}
