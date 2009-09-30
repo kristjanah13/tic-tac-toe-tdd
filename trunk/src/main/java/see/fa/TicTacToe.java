ed7736f7e8d7a785cfe265d7c7ac80901726bb9f
franz.see <franz.see@b2682abc-adb6-11de-9e5f-e55ec289a8a8>
2009-09-30 16:34:27 +0000

    Added implementation to TicTacToe#move(..) for vertical win checking.

--- trunk/src/main/java/see/fa/TicTacToe.java
+++ trunk/src/main/java/see/fa/TicTacToe.java
@@ -12,8 +12,17 @@ public class TicTacToe {
 	public Result move(Position position, Mark mark) {
 		board.set(position, mark);
 		
-		Row row = board.getRow(position);
-		return Result.valueOf(row);
+		Result rowResult = Result.valueOf(board.getRow(position));
+		if(rowResult != Result.GAME_NOT_FINISH) {
+			return rowResult;
+		}
+		
+		Result colResult = Result.valueOf(board.getCol(position));
+		if(rowResult != Result.GAME_NOT_FINISH) {
+			return colResult;
+		}
+		
+		return Result.GAME_NOT_FINISH; 
 	}
 
 	public Board getBoard() {
