95ec827e16604c5409a779733d468b3f8b3b93a8
franz.see <franz.see@b2682abc-adb6-11de-9e5f-e55ec289a8a8>
2009-10-04 15:14:32 +0000

    [RED] Refactored test by creating a Board TestSuite and grouping the tests by features.

--- src/test/java/org/tdd/samples/tictactoe/BoardTest.java
+++ src/test/java/org/tdd/samples/tictactoe/BoardTest.java
@@ -1,42 +1,55 @@
 package org.tdd.samples.tictactoe;
 
+import junit.framework.Test;
 import junit.framework.TestCase;
+import junit.framework.TestSuite;
 
-public class BoardTest extends TestCase {
+public class BoardTest extends TestSuite {
 	
-	private Board board;
-	
-	@Override
-	protected void setUp() throws Exception {
-		super.setUp();
-		board = new Board();
+	public static Test suite() {
+		TestSuite allTests = new TestSuite();
+		return allTests;
 	}
+	
+	public static class AbstractBoardTest extends TestCase {
+		protected Board board;
+		
+		@Override
+		protected void setUp() throws Exception {
+			super.setUp();
+			board = new Board();
+		}
 
-	@Override
-	protected void tearDown() throws Exception {
-		board = null;
-		super.tearDown();
-	}
-
-	public void testGivenMarkedPosition_WhenSet_ThenUpdateBoard() {
-		MarkedPosition givenMarkedPosition = new MarkedPosition(1, 1, Mark.X);
-		
-		board.set(givenMarkedPosition);
-		
-		assertEquals("Should have marked the position.", 
-				new Board(new Mark[][]{
-						{Mark.X, Mark.NONE, Mark.NONE}, 
-						{Mark.NONE, Mark.NONE, Mark.NONE}, 
-						{Mark.NONE, Mark.NONE, Mark.NONE}
-				}), 
-				board);
+		@Override
+		protected void tearDown() throws Exception {
+			board = null;
+			super.tearDown();
+		}
 	}
 	
-	public void testGivenPositionIsAlreadyOccupied_WhenContains_ThenReturnTrue() {
-		
+	public static class WhenSet extends AbstractBoardTest {
+		public void testShouldUpdateBoard() {
+			MarkedPosition givenMarkedPosition = new MarkedPosition(1, 1, Mark.X);
+			
+			board.set(givenMarkedPosition);
+			
+			assertEquals("Should have marked the position.", 
+					new Board(new Mark[][]{
+							{Mark.X, Mark.NONE, Mark.NONE}, 
+							{Mark.NONE, Mark.NONE, Mark.NONE}, 
+							{Mark.NONE, Mark.NONE, Mark.NONE}
+					}), 
+					board);
+		}
 	}
 	
-	public void testGivenPositionIsNoetOccupied_WhenContains_ThenReturnFalse() {
+	public static class WhenContains extends AbstractBoardTest {
+		public void testGivenPositionIsAlreadyOccupied_ThenReturnTrue() {
+			
+		}
 		
+		public void testGivenPositionIsNotYetOccupied_ThenReturnFalse() {
+			
+		}
 	}
 }
