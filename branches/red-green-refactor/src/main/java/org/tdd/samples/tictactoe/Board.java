354efe67ed7bdfa7e1647ce4550bf947bac50bd8
franz.see <franz.see@b2682abc-adb6-11de-9e5f-e55ec289a8a8>
2009-10-04 14:29:08 +0000

    [GREEN] Fixed compile failure and made the test pass.

--- branches/red-green-refactor/src/main/java/org/tdd/samples/tictactoe/Board.java
+++ branches/red-green-refactor/src/main/java/org/tdd/samples/tictactoe/Board.java
@@ -1,17 +1,67 @@
 package org.tdd.samples.tictactoe;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
 public class Board {
 	
+	private static final String ROW_SEPARATOR = "\n";
+	private static final String COL_SEPARATOR = " ";
+	private final Mark[][] marks;
+	
 	public Board() {
-		
+		this(new Mark[][]{
+				{Mark.NONE, Mark.NONE, Mark.NONE},
+				{Mark.NONE, Mark.NONE, Mark.NONE},
+				{Mark.NONE, Mark.NONE, Mark.NONE}
+		});
 	}
 
 	public Board(Mark[][] marks) {
+		this.marks = marks;
 	}
 
 	public void set(MarkedPosition markedPosition) {
-		// TODO Auto-generated method stub
-		
+		marks[markedPosition.getEffectiveRow()][markedPosition.getEffectiveColumn()] = markedPosition.getMark();
 	}
 
+	@Override
+	public boolean equals(Object obj) {
+		if(obj == null || !obj.getClass().isAssignableFrom(Board.class)) {
+			return false;
+		}
+		
+		Board that = (Board) obj;
+		
+		EqualsBuilder equalsBuilder = new EqualsBuilder();
+		equalsBuilder.append(this.marks, that.marks);
+		return equalsBuilder.isEquals();
+	}
+
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder()
+			.append(marks)
+			.toHashCode();
+	}
+
+	@Override
+	public String toString() {
+		List<String> marksPerRow = new LinkedList<String>();
+		for(Mark[] row : marks) {
+			marksPerRow.add(StringUtils.join(row, COL_SEPARATOR));
+		}
+
+		return new StringBuilder()
+			.append("{")
+			.append("\n")
+			.append(StringUtils.join(marksPerRow, ROW_SEPARATOR))
+			.append("}").toString();
+	}
+
+	
 }
--- branches/red-green-refactor/src/main/java/org/tdd/samples/tictactoe/MarkedPosition.java
+++ branches/red-green-refactor/src/main/java/org/tdd/samples/tictactoe/MarkedPosition.java
@@ -2,8 +2,26 @@ package org.tdd.samples.tictactoe;
 
 public class MarkedPosition {
 
+	private final int col;
+	private final int row;
+	private final Mark mark;
+
 	public MarkedPosition(int row, int col, Mark mark) {
-		
+		this.row = row;
+		this.col = col;
+		this.mark = mark;
+	}
+
+	public int getEffectiveColumn() {
+		return col - 1;
+	}
+
+	public int getEffectiveRow() {
+		return row - 1;
+	}
+
+	public Mark getMark() {
+		return mark;
 	}
 
 }
