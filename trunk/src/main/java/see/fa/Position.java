d016e75c4f9d150dcd8b1164d5e0e93d387c7ef0
franz.see <franz.see@b2682abc-adb6-11de-9e5f-e55ec289a8a8>
2009-10-04 08:53:21 +0000

    Added identity methods.

--- trunk/src/main/java/see/fa/Position.java
+++ trunk/src/main/java/see/fa/Position.java
@@ -1,5 +1,9 @@
 package see.fa;
 
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+
 public class Position {
 
 	private final int x;
@@ -17,5 +21,34 @@ public class Position {
 	public int getY() {
 		return y;
 	}
-	
+
+	@Override
+	public boolean equals(Object obj) {
+		if(obj == null || !Position.class.isAssignableFrom(obj.getClass())) {
+			return false;
+		}
+		
+		Position that = (Position) obj;
+		return new EqualsBuilder()
+			.append(this.x, that.x)
+			.append(this.y, that.y)
+			.isEquals();
+	}
+
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder()
+			.append(x)
+			.append(y)
+			.toHashCode();
+	}
+
+	@Override
+	public String toString() {
+		return new ToStringBuilder(this)
+			.append(x)
+			.append(y)
+			.toString();
+	}
+
 }
