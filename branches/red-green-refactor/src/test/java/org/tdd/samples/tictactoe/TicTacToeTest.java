e55b1862320b94c88c0cc52c5dd0420358997fda
franz.see <franz.see@b2682abc-adb6-11de-9e5f-e55ec289a8a8>
2009-10-04 10:01:45 +0000

    [UNDO] Undid refactor on TicTacToeTest.

--- branches/red-green-refactor/src/test/java/org/tdd/samples/tictactoe/TicTacToeTest.java
+++ branches/red-green-refactor/src/test/java/org/tdd/samples/tictactoe/TicTacToeTest.java
@@ -1,113 +1,77 @@
 package org.tdd.samples.tictactoe;
 
-import junit.framework.Test;
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
-public class TicTacToeTest extends TestSuite {
-	
-	public static Test suite() {
-		TestSuite allTests = new TestSuite();
-		allTests.addTestSuite(GivenNewGame_WhenMove.class);
-		allTests.addTestSuite(GivenRunningGame_WhenMove.class);
-		allTests.addTestSuite(GivenRunningGame_AndLastMoveIsXMark_WhenMove.class);
-		allTests.addTestSuite(GivenRunningGame_AndLastMoveIsOMark_WhenMove.class);
-		allTests.addTestSuite(GivenEndGame_WhenMove.class);
-		return allTests;
+public class TicTacToeTest extends TestCase {
+
+	private TicTacToe ticTacToe;
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		ticTacToe = new TicTacToe();
 	}
 
-	public static class AbstractTicTacToeTest extends TestCase {
-		protected TicTacToe ticTacToe;
-
-		@Override
-		protected void setUp() throws Exception {
-			super.setUp();
-			ticTacToe = new TicTacToe();
-		}
-
-		@Override
-		protected void tearDown() throws Exception {
-			ticTacToe = null;
-			super.tearDown();
-		}		
+	@Override
+	protected void tearDown() throws Exception {
+		ticTacToe = null;
+		super.tearDown();
 	}
 	
-	public static class GivenNewGame_WhenMove extends AbstractTicTacToeTest {
-		public void testGivenAMarkedPosition_ThenMarkPositionOnBoard() {
-			
-		}
-		
-		public void test_ThenDoThrowGameOverException() {
-			
-		}
-	}
-	
-	public abstract static class AbstractGivenRunningGame_WhenMove extends AbstractTicTacToeTest {
+	public void testGivenAMark_AndAPositionThatIsNotOccupied_WhenMove_ThenMarkPositionOnBoard() {
 		
 	}
 
-	public static class GivenRunningGame_WhenMove extends AbstractGivenRunningGame_WhenMove {
-		public void testGivenAMarkedPositionThatIsNotYetOccupied_ThenDotNotThrowIllegalMoveException() {
-			
-		}
-
-		public void testGivenAMarkedPositionThatIsAlreadyOccupied_ThenThrowIllegalMoveException() {
-			
-		}
+	public void testGivenAMark_AndAPositionThatIsOccupied_WhenMove_ThenMarkPositionOnBoard() {
 		
-		public void testGivenAMarkedPositionWithinTheBoard_WhenMove_ThenDoNotThrowIllegalMoveException() {
-			
-		}
-		
-		public void test_ThenDoThrowGameOverException() {
-			
-		}
-		
-		public void testGivenMarkedPositionWhichMakesAHorizontalLine_ThenWin() {
-			
-		}
-		
-		public void testGivenMarkedPositionWhichMakesAVerticalLine_ThenWin() {
-			
-		}
-		
-		public void testGivenMarkedPositionWhichMakesAnUpperLeftToLowerRightDiagonalLine_ThenWin() {
-			
-		}
-		
-		public void testGivenMarkedPositionWhichMakesAnUpperRightToLowerLeftDiagonalLine_ThenWin() {
-			
-		}
-		
-		public void testGivenMarkedPositionWhichMakesNoStraightLine_ThenContinueGame() {
-			
-		}
-	}
-
-	public static class GivenRunningGame_AndLastMoveIsXMark_WhenMove extends AbstractGivenRunningGame_WhenMove {
-		public void testGivenAnXMark_ThenThrowIllegalMoveException() {
-			
-		}
-		
-		public void testGivenAnOMark_ThenDoNotThrowIllegalMoveException() {
-			
-		}
 	}
 	
-	public static class GivenRunningGame_AndLastMoveIsOMark_WhenMove extends AbstractGivenRunningGame_WhenMove {
-		public void testGivenAnXMark_ThenDoNotThrowIllegalMoveException() {
-			
-		}
+	public void testGivenAnXMark_AndNoPreviousMove_WhenMove_ThenDoNotThrowIllegalMoveException() {
 		
-		public void testGivenAnOMark_ThenThrowIllegalMoveException() {
-			
-		}
 	}
 	
-	public static class GivenEndGame_WhenMove extends AbstractTicTacToeTest {
-		public void test_ThenThrowGameOverException() {
-			
-		}
+	public void testGivenAnXMark_AndPreviousMoveIsOMark_WhenMove_ThenDoNotThrowIllegalMoveException() {
+		
+	}
+	
+	public void testGivenAnXMark_AndPreviousMoveIsXMark_WhenMove_ThenThrowIllegalMoveException() {
+		
+	}
+	
+	public void testGivenAnOMark_AndNoPreviousMove_WhenMove_ThenDoNotThrowIllegalMoveException() {
+		
+	}
+	
+	public void testGivenAnOMark_AndPreviousMoveIsXMark_WhenMove_ThenDoNotThrowIllegalMoveException() {
+		
+	}
+	
+	public void testGivenAnOMark_AndPreviousMoveIsOMark_WhenMove_ThenThrowIllegalMoveException() {
+		
+	}
+	
+	public void testGivenMarkedPositionWhichMakesAHorizontalLine_WhenMove_ThenWin() {
+		
+	}
+	
+	public void testGivenMarkedPositionWhichMakesAVerticalLine_WhenMove_ThenWin() {
+		
+	}
+	
+	public void testGivenMarkedPositionWhichMakesAnUpperLeftToLowerRightDiagonalLine_WhenMove_ThenWin() {
+		
+	}
+	
+	public void testGivenMarkedPositionWhichMakesAnUpperRightToLowerLeftDiagonalLine_WhenMove_ThenWin() {
+		
+	}
+	
+	public void testGivenMarkedPositionWhichMakesNoStraightLine_WhenMove_ThenContinueGame() {
+		
+	}
+	
+	public void testGivenMarkedPositionWhileGameHasEnded_WhenMove_ThenThrowGameOverException() {
+		
 	}
 	
 }
