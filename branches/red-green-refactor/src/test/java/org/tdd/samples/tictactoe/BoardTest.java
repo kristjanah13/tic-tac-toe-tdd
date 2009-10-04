8d528d0356959c47a4ebcddb803055adc343f48c
franz.see <franz.see@b2682abc-adb6-11de-9e5f-e55ec289a8a8>
2009-10-04 14:12:40 +0000

    [RED] Added test for Board#set(..).

---
+++ branches/red-green-refactor/src/test/java/org/tdd/samples/tictactoe/BoardTest.java
@@ -0,0 +1,34 @@
+package org.tdd.samples.tictactoe;
+
+import junit.framework.TestCase;
+
+public class BoardTest extends TestCase {
+	
+	private Board board;
+	
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		board = new Board();
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		board = null;
+		super.tearDown();
+	}
+
+	public void testGivenMarkedPosition_WhenSet_ThenUpdateBoard() {
+		MarkedPosition givenMarkedPosition = new MarkedPosition(1, 1, Mark.X);
+		
+		board.set(givenMarkedPosition);
+		
+		assertEquals("Should have marked the position.", 
+				new Board(new Mark[][]{
+						{Mark.X, Mark.NONE, Mark.NONE}, 
+						{Mark.NONE, Mark.NONE, Mark.NONE}, 
+						{Mark.NONE, Mark.NONE, Mark.NONE}
+				}), 
+				board);
+	}
+}
