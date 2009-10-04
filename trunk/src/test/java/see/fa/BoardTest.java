a70b1af1e3befa78af4b6560333e5c551fadf2d7
franz.see <franz.see@b2682abc-adb6-11de-9e5f-e55ec289a8a8>
2009-10-04 08:51:52 +0000

    Added test for diagonal win.

--- trunk/src/test/java/see/fa/BoardTest.java
+++ trunk/src/test/java/see/fa/BoardTest.java
@@ -1,5 +1,10 @@
 package see.fa;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 import junit.framework.TestCase;
 
 public class BoardTest extends TestCase {
@@ -34,5 +39,27 @@ public class BoardTest extends TestCase {
 		Line colAtPosition = board.getCol(new Position(1,1));
 		
 		assertEquals("Should have retrieved 1st col.", new Line(Mark.X, Mark.NONE, Mark.O), colAtPosition);
+	}
+	
+	public void testGivenAPositionForUpperLeftToLowerRight_WhenGetDiag_ThenReturnLineContainingTheMarksOfThatDiag() {
+		Set<Line> diagonals = board.getDiags(new Position(1,1));
+		
+		assertEquals("Should have retrieved upper left to lower right diagonal line.", Collections.singleton(new Line(Mark.X, Mark.X, Mark.X)), diagonals);
+	}
+	
+	public void testGivenAPositionForUpperRightToLowerLeft_WhenGetDiag_ThenReturnLineContainingTheMarksOfThatDiag() {
+		Set<Line> diagonals = board.getDiags(new Position(3,1));
+		
+		assertEquals("Should have retrieved upper right to lower left diagonal line.", Collections.singleton(new Line(Mark.NONE, Mark.X, Mark.O)), diagonals);
+	}
+	
+	public void testGivenCenterPosition_WhenGetDiag_ThenReturnBothDiagLines() {
+		Set<Line> diagonals = board.getDiags(new Position(2,2));
+		
+		assertEquals("Should have retrieved both diagonal lines.", 
+				new HashSet<Line>(Arrays.asList(
+						new Line(Mark.X, Mark.X, Mark.X), 
+						new Line(Mark.NONE, Mark.X, Mark.O)
+					)), diagonals);
 	}
 }
