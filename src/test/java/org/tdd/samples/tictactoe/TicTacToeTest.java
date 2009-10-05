ae5b4a83d4148e91d8fa221b0c58786a32f8da44
franz.see <franz.see@b2682abc-adb6-11de-9e5f-e55ec289a8a8>
2009-10-05 13:37:06 +0000

    [RED] Added test implementation for testGivenMarkedPositionWhichMakesNoStraightLine_ThenContinueGame.

--- src/test/java/org/tdd/samples/tictactoe/TicTacToeTest.java
+++ src/test/java/org/tdd/samples/tictactoe/TicTacToeTest.java
@@ -170,7 +170,9 @@ public class TicTacToeTest extends TestSuite {
 		}
 		
 		public void testGivenMarkedPositionWhichMakesNoStraightLine_ThenContinueGame() {
+			Result result = ticTacToe.move(new MarkedPosition(2, 3, nextMark));
 			
+			assertEquals("X Should have won.", Result.GAME_IS_ONGOING, result);
 		}
 	}
 
