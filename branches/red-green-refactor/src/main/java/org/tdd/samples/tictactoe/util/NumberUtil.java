4f82b13183072f4a53ce2d971c4cee3ce375c10e
franz.see <franz.see@b2682abc-adb6-11de-9e5f-e55ec289a8a8>
2009-10-24 18:47:10 +0000

    [RED] Fixed compile error but tests still fail.

---
+++ branches/red-green-refactor/src/main/java/org/tdd/samples/tictactoe/util/NumberUtil.java
@@ -0,0 +1,12 @@
+package org.tdd.samples.tictactoe.util;
+
+import java.util.List;
+
+public class NumberUtil {
+	
+	public enum Order { Ascending, Descending };
+
+	public static Order getOrder(List<? extends Number> numbers) {
+		return null;
+	}
+}
