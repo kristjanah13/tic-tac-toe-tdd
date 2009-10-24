0f2f37bfcf5574428d282e1722d102075cf38b41
franz.see <franz.see@b2682abc-adb6-11de-9e5f-e55ec289a8a8>
2009-10-24 17:05:15 +0000

    [RED] Added checking if line is marked the same.

--- src/main/java/org/tdd/samples/tictactoe/TicTacToe.java
+++ src/main/java/org/tdd/samples/tictactoe/TicTacToe.java
@@ -30,7 +30,7 @@ public class TicTacToe {
 		
 		Line line = board.getRow(markedPosition.getPosition());
 		Result result;
-		if(line.isStraight()) {
+		if(line.isStraight() && line.isMarkedTheSame()) {
 			result = markedPosition.getMark() == Mark.X ? Result.X_WINS : Result.O_WINS;
 		} else {
 			result = Result.GAME_IS_ONGOING;
