ec8ebd46898d8b86d29399ac6271670a3d0f74cb
franz.see <franz.see@b2682abc-adb6-11de-9e5f-e55ec289a8a8>
2009-10-24 18:54:01 +0000

    [RED] Made NumberUtilTest pass (but other tests still fail).

--- src/main/java/org/tdd/samples/tictactoe/util/NumberUtil.java
+++ src/main/java/org/tdd/samples/tictactoe/util/NumberUtil.java
@@ -7,6 +7,28 @@ public class NumberUtil {
 	public enum Order { Ascending, Descending };
 
 	public static Order getOrder(List<? extends Number> numbers) {
-		return null;
+		return isAscending(numbers) ? Order.Ascending :
+				isDescending(numbers) ? Order.Descending :
+				null;
+	}
+
+	private static boolean isAscending(List<? extends Number> numbers) {
+		boolean ascending = true;
+		int i = 0;
+		while(ascending && i < numbers.size() - 1) {
+			ascending = numbers.get(i).longValue()+1 == numbers.get(i+1).longValue();
+			i++;
+		}
+		return ascending;
+	}
+	
+	private static boolean isDescending(List<? extends Number> numbers) {
+		boolean descending = true;
+		int i = 0;
+		while(descending && i < numbers.size() - 1) {
+			descending = numbers.get(i).longValue()-1 == numbers.get(i+1).longValue();
+			i++;
+		}
+		return descending;
 	}
 }
