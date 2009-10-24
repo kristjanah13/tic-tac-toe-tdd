ba0455f4e84de83039f378686fb4caa05bed31e1
franz.see <franz.see@b2682abc-adb6-11de-9e5f-e55ec289a8a8>
2009-10-24 16:32:12 +0000

    [RED] Made testShouldReturnMarksInTheBoardWithTheSameRow pass but other tests still fail.

--- branches/red-green-refactor/src/main/java/org/tdd/samples/tictactoe/Line.java
+++ branches/red-green-refactor/src/main/java/org/tdd/samples/tictactoe/Line.java
@@ -1,13 +1,43 @@
 package org.tdd.samples.tictactoe;
 
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ReflectionToStringBuilder;
+
 public class Line {
 
+	private final Position[] positions;
+
 	public Line(Position[] positions) {
-		
+		this.positions = positions;
 	}
 
 	public boolean areAllTheSame() {
 		return false;
 	}
 
+	@Override
+	public boolean equals(Object obj) {
+		if(obj == null || !Line.class.isAssignableFrom(obj.getClass())) {
+			return false;
+		}
+		
+		Line that = (Line)obj;
+		return new EqualsBuilder()
+			.append(this.positions, that.positions)
+			.isEquals();
+	}
+
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder()
+		    .append(positions)
+		    .toHashCode();
+	}
+
+	@Override
+	public String toString() {
+		return ReflectionToStringBuilder.toString(this);
+	}
+	
 }
