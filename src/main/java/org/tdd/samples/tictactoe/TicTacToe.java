82f8c1eedc795315e04c0e77fdce164d80a5eba7
franz.see <franz.see@b2682abc-adb6-11de-9e5f-e55ec289a8a8>
2009-10-25 01:33:09 +0000

    [RED] Added diagonal-win checking in TicTacToe.

--- src/main/java/org/tdd/samples/tictactoe/TicTacToe.java
+++ src/main/java/org/tdd/samples/tictactoe/TicTacToe.java
@@ -1,5 +1,9 @@
 package org.tdd.samples.tictactoe;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 public class TicTacToe {
 
 	private final Board board;
@@ -28,15 +32,20 @@ public class TicTacToe {
 		
 		board.set(markedPosition);
 		
-		Line row = board.getRow(markedPosition.getPosition());
-		Line column = board.getColumn(markedPosition.getPosition());
-		Result result;
-		if(row.isMarkedTheSame() || column.isMarkedTheSame()) {
-			result = markedPosition.getMark() == Mark.X ? Result.X_WINS : Result.O_WINS;
-		} else {
-			result = Result.GAME_IS_ONGOING;
-		}
+		Set<Line> allLines = new HashSet<Line>();
+		allLines.add(board.getRow(markedPosition.getPosition()));
+		allLines.add(board.getColumn(markedPosition.getPosition()));
+		allLines.addAll( board.getDiagonals(markedPosition.getPosition()));
 		
+		Result result = Result.GAME_IS_ONGOING;
+		Iterator<Line> i = allLines.iterator();
+		while(result == Result.GAME_IS_ONGOING && i.hasNext()) {
+			Line line = i.next();
+			if(line.isMarkedTheSame()) {
+				result = markedPosition.getMark() == Mark.X ? Result.X_WINS : Result.O_WINS;
+			}
+		} 
+
 		return result;
 	}
 
